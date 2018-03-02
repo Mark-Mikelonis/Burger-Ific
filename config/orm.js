12c08c2c74d867f7f57b5e6d6e55ac5007af9083
Mark Mikelonis <mark.mikelonis@gmail.com>
2018-03-01 21:48:24 -0800

    hooked up handlebars

--- config/orm.js
+++ config/orm.js
@@ -3,22 +3,25 @@ var connection = require("./connection.js");
 
 
 var orm = {
-    selectAll: function(tableName){
+    selectAll: function(tableName, cb){
         var query = "SELECT * FROM ??";
         connection.query(query, [tableName], function(err, result){
            console.log(result);
+           cb(result);
         });
     },
-    insertOne: function(tableName, burger_name, devoured ){
+    insertOne: function(tableName, burger_name, devoured, cb){
         var query = "INSERT INTO ?? ('burger_name', 'devoured')  VALUES (?,?) ";
         connection.query(query, [tableName, burger_name, devoured], function(err, result){
             console.log(result);
+            cb(result);
         });
     },
-    updateOne: function(tableName,id, devoured){
+    updateOne: function(tableName,id, devoured, cb){
         var query = "UPDATE ?? SET devoured = ? WHERE id= ?";
         connection.query(query, [tableName,devoured,id], function(err,result){
             console.log(result);
+            cb(result);
         });
     }
 }
